f26462474fccc55594f4b6f1d133922ed846abf0
abdulwhab241 <abdulwhab241@gmail.com>
2022-11-30 14:12:39 -0500

    fidfdfdf

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/ping', function () {
+    return 'ping';
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
